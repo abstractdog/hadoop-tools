9cff2417d8ef8b30f641ab7044177f6f880e3431
root <root@dev-lbodor-hiveontez-1.vpc.cloudera.com>
2019-11-30 00:52:56 +0100

    java detection

--- hdfs-shell/bin/hdfs-shell.sh
+++ hdfs-shell/bin/hdfs-shell.sh
@@ -4,4 +4,4 @@ HADOOP_DIR=${HADOOP_CONF_DIR:-/etc/hadoop/conf}
 
 echo "Launching HDFS Shell with HADOOP_CONF_DIR: ${HADOOP_DIR}"
 
-java -Xms200m -Xmx400m -cp ${SCRIPT_DIR}/../lib/*:${HADOOP_DIR} com.avast.server.hdfsshell.MainApp "$@"
+$JAVA_EXEC -Xms200m -Xmx400m -cp ${SCRIPT_DIR}/../lib/*:${HADOOP_DIR} com.avast.server.hdfsshell.MainApp "$@"
--- init.sh
+++ init.sh
@@ -2,3 +2,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH=$PATH:$SCRIPT_DIR/hdfs-shell/bin
 export PATH=$PATH:$SCRIPT_DIR/yarn
+
+which java > /dev/null 2>&1
+if [ $? -gt 0 ]; then
+    echo -n "failed to find java, searching it under /usr..."
+    export JAVA_EXEC=$(find /usr -executable -type f -name java | head -n 1)
+    echo $JAVA_EXEC
+else
+    export JAVA_EXEC=$(which java)
+fi
+
