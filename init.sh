f4abce6cf55910d64739bb59060c98176bb50b65
abstractdog <bodorlaszlo0202@gmail.com>
2019-11-30 12:52:55 +0100

    init.sh: remove echoes

--- init.sh
+++ init.sh
@@ -3,11 +3,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$SCRIPT_DIR/hdfs-shell/bin
 export PATH=$PATH:$SCRIPT_DIR/yarn
 
+#this script is called from .bashrc, don't use echo as it can break scp
+#https://bugzilla.redhat.com/show_bug.cgi?id=20527
+
 which java > /dev/null 2>&1
 if [ $? -gt 0 ]; then
-    echo -n "failed to find java, searching it under /usr..."
+    #echo -n "failed to find java, searching it under /usr..."
     export JAVA_EXEC=$(find /usr -executable -type f -name java | head -n 1)
-    echo $JAVA_EXEC
+    #echo $JAVA_EXEC
 else
     export JAVA_EXEC=$(which java)
 fi
