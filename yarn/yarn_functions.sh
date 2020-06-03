22bb068fc9a9edbe036dceaa277a8eba5cc06473
root <root@ctr-e141-1563959304486-69251-01-000003.hwx.site>
2020-06-03 09:57:26 +0000

    new yarn functions

--- yarn/yarn_functions.sh
+++ yarn/yarn_functions.sh
@@ -1,6 +1,7 @@
 
 function yarn_poll_app_logs () {
   APP=$1
+  AS_USER=$2
 
   echo "polling application logs: $APP"
   success=1
@@ -10,11 +11,38 @@ function yarn_poll_app_logs () {
     counter=$(( $counter + 1 ))
     #clear
     echo "...fetching app logs, attempt: $counter..."
-    yarn logs -applicationId $APP --size_limit_mb -1 > /dev/null
+    if [ -z "$AS_USER" ]; then
+      yarn logs -applicationId $APP --size_limit_mb -1 > /dev/null
+    else
+      sudo -u $AS_USER yarn logs -applicationId $APP --size_limit_mb -1 > /dev/null
+    fi
     success=$?
   done
+
   echo "saving application logs to $APP.log"
-  yarn logs -applicationId $APP --size_limit_mb -1 > $APP.log
+  if [ -z "$AS_USER" ]; then
+    yarn logs -applicationId $APP --size_limit_mb -1 > $APP.log
+  else
+    sudo -u $AS_USER yarn logs -applicationId $APP --size_limit_mb -1 > $APP.log
+  fi
 
   less $APP.log
+}
+
+function yarn_app_container_list(){
+  APP=$1
+  yarn logs -applicationId $APP -show_application_log_info
+}
+
+function yarn_kill_hive_applications(){
+  yarn application -list | awk '$2 ~ "HIVE" { print $1 }' | xargs -I {} yarn application -kill {}
+}
+
+function yarn_list_llap_applications(){
+  yarn application -list | awk '$2 ~ "llap" { print $1,$2 }'
+}
+
+function yarn_kill_llap_applications(){
+  yarn_list_llap_applications
+  yarn application -list | awk '$2 ~ "llap" { print $1 }' | xargs -I {} yarn application -kill {}
 }
