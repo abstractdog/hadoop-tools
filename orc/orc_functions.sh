fe49912bd9e5f9ae9e93f8e06912a420bc619717
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2020-06-22 19:10:37 +0200

    orc filedump meta+data

--- orc/orc_functions.sh
+++ orc/orc_functions.sh
@@ -1,4 +1,12 @@
+
+#http://www.mtitek.com/tutorials/bigdata/hadoop/orc-tools.php
 function orc_filedump () {
   FILE=$1
-  $JAVA_EXEC -jar $ORC_UBER_JAR data $FILE
+  if [[ -z $FILE ]]; then
+    echo "please provide an orc file as first parameter"
+    #$JAVA_EXEC -jar $ORC_UBER_JAR --help
+  else
+    $JAVA_EXEC -jar $ORC_UBER_JAR meta $FILE
+    $JAVA_EXEC -jar $ORC_UBER_JAR data $FILE
+  fi
 }
